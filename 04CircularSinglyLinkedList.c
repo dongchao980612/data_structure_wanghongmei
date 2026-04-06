f617e7b99372bd3af47063d60ce73c021306db4c
dongchao612 <dongchao980612@163.com>
2026-04-06 15:03:28 +0800

    add  double link list

---
+++ 04CircularSinglyLinkedList.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int DataType;
+
+#define OK  0
+#define ERR -1
+
+typedef struct Node {
+	int data;
+	struct Node *next;
+} Node, *CLinkList;
+
+int main(){
+	
+	
+	return 0;
+}
---
+++ 05CircularDoublyLinkedList.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int DataType;
+
+#define OK  0
+#define ERR -1
+
+typedef struct DNode {
+	int data;
+	struct DNode *prior;
+	struct DNode *next;
+} DNode, *CDLinkList;
+
+int main(){
+	
+	
+	return 0;
+}
